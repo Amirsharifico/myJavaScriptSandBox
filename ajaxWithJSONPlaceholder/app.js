391a23db621e909e066293d36b9599419df44f43
amirsharifico <amirsharifico@gmail.com>
2023-02-13 14:26:04 -0500

    AJAX from an external API

---
+++ ajaxWithJSONPlaceholder/app.js
@@ -0,0 +1,30 @@
+document.getElementById('get-post').addEventListener('click', loadPosts)
+
+function loadPosts() {
+    const xhr = new XMLHttpRequest();
+
+    const number = document.getElementById('number-post').value;
+
+    
+    xhr.open("GET", `https://jsonplaceholder.typicode.com/posts/${number}`);
+  
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        let post = JSON.parse(xhr.responseText);
+
+       
+          output = `
+                  <ul>
+                      <li>ID : ${post.id}</li>
+                      <li>Title : ${post.title}</li>
+                      <li>Body : ${post.body}</li>
+                  </ul>
+                  `;
+        
+  
+        document.getElementById('output').innerHTML = output;
+      }
+    };
+  
+    xhr.send();
+  }
